382a6d58b98288996c2e172d9c968787a7851e96
Larissa Santos <larrissapcs@hotmail.com>
2016-10-01 18:58:49 -0300

    Alterando layout de trabalhos e adicionando chamada de ajax para envio de email

--- js/controllers/enacompCtrl.js
+++ js/controllers/enacompCtrl.js
@@ -1,10 +1,23 @@
-angular.module("enacomp",["ngRoute"]);
+angular.module("enacomp",["ngRoute", 'angular-loading-bar'])
+.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = false;
+     // cfpLoadingBarProvider.includeBar = false;
+ }]);
 
-angular.module("enacomp").controller("headerCtrl", ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
+angular.module("enacomp").controller("headerCtrl", ['$scope', '$location', '$anchorScroll', 'cfpLoadingBar', function($scope, $location, $anchorScroll, cfpLoadingBar) {
     $scope.gotoTop = function() {
 		$location.hash('top');
 		$anchorScroll();
     };
+
+    $scope.start = function() {
+      cfpLoadingBar.start();
+    };
+
+    $scope.complete = function () {
+      cfpLoadingBar.complete();
+    }
+
 }]);
 
 angular.module("enacomp").controller("tabsCtrl", ['$scope', function($scope) {
@@ -19,12 +32,27 @@ angular.module("enacomp").controller("tabsCtrl", ['$scope', function($scope) {
     };
 }]);
 
-angular.module("enacomp").controller('contatoCtrl', function($scope) {
+angular.module("enacomp").controller('contatoCtrl', function($scope, $http) {
 
     $scope.submitForm = function() {
         if ($scope.userForm.$valid) {
         	console.log($scope.user);
-        }
+        
+	        $http({
+				method: 'POST',
+				url: 'requisicoes/sendEmail.php',
+				data:  $.param($scope.user),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).then(function successCallback(response) {
+				console.log('ok'+response);
+			    // this callback will be called asynchronously
+			    // when the response is available
+			}, function errorCallback(response) {
+				console.log('erro'+response);
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			});
+		}
 
     };
 
