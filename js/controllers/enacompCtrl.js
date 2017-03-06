94aa930ff4ac2cd25140916628ec203a7ec0d873
Larissa Santos <larrissapcs@hotmail.com>
2017-03-06 20:56:13 -0300

    Alterando programacao

--- js/controllers/enacompCtrl.js
+++ js/controllers/enacompCtrl.js
@@ -6,8 +6,10 @@ angular.module("enacomp",["ngRoute", 'angular-loading-bar'])
 
 angular.module("enacomp").controller("headerCtrl", ['$scope', '$location', '$anchorScroll', 'cfpLoadingBar', function($scope, $location, $anchorScroll, cfpLoadingBar) {
     $scope.gotoTop = function() {
-		$location.hash('top');
-		$anchorScroll();
+		  $location.hash('top');
+		  $anchorScroll();
+      // $('.navbar-toggle').removeClass('collapsed');
+      
     };
 
     $scope.start = function() {
@@ -30,31 +32,4 @@ angular.module("enacomp").controller("tabsCtrl", ['$scope', function($scope) {
     $scope.isSet = function(tabNum){
       return $scope.tab === tabNum;
     };
-}]);
-
-angular.module("enacomp").controller('contatoCtrl', ['$scope', '$http', function($scope, $http) {
-
-    $scope.submitForm = function() {
-        if ($scope.userForm.$valid) {
-        	console.log($scope.user);
-        
-	        /*$http({
-				method: 'POST',
-				url: 'requisicoes/sendEmail.php',
-				data:  $.param($scope.user),
-				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-			}).then(function successCallback(response) {
-				console.log('ok'+response);
-			    // this callback will be called asynchronously
-			    // when the response is available
-			}, function errorCallback(response) {
-				console.log('erro'+response);
-			    // called asynchronously if an error occurs
-			    // or server returns response with an error status.
-			});
-			*/
-		}
-
-    };
-
 }]);
